79875a60f72ab759cc6984493f028df80da46e4d
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-05-07 04:51:49 -0600

    Added more Classes parctice

--- ClassesPractice/Program.cs
+++ ClassesPractice/Program.cs
@@ -2,28 +2,15 @@
 
 namespace ClassesPractice
 {
-    public class Person
-    {
-        public string Name;
-
-        public void Introduce(string to)
-        {
-            Console.WriteLine(" Hi {0}, I am {1}", to, Name);
-        }
-        public static Person Parse(string str)
-        {
-            var person = new Person();
-            person.Name = str;
-            return person;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            
-            var person = Person.Parse("Jon");
-            person.Introduce("Hayder");
+            var customer = new Customer(1, "Haider");
+            var order = new Order();
+            customer.Orders.Add(order);
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
         }
     }
 }
